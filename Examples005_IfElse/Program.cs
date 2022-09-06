f912d8dcc3f7fc4e8eae6b98a3b874fee9f94e4f
Anton <antslyusar@gmail.com>
2022-09-06 19:58:07 +0700

    Example005

---
+++ Examples005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray! It's MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
